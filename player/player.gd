3f048e436ba1d33bfe99ec168a6c98319446e355
ConnerGordon <2260684@flourbluffschools.org>
2026-01-13 12:12:29 -0600

    particl

--- player/player.gd
+++ player/player.gd
@@ -4,10 +4,14 @@ class_name Player
 @export_range(750,2500) var force := 1000.0
 @export var torque_thrust := 100
 
+@onready var mainbooster: GPUParticles3D = $mainthrust
+@onready var leftboost: GPUParticles3D = $leftboost
+
 var transitioning := false
 @onready var explosionaudio: AudioStreamPlayer = $explosionaudio
 @onready var successaudio: AudioStreamPlayer = $successaudio
 @onready var rocketaudio: AudioStreamPlayer3D = $rocketaudio
+@onready var rightboost: GPUParticles3D = $rightboost
 
 
 
@@ -17,15 +21,23 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not transitioning:
 		if Input.is_action_pressed("boost"):
+			mainbooster.emitting = true
 			apply_central_force(basis.y*delta*force)
 			if not rocketaudio.is_playing():
 				rocketaudio.play()
 		else:
 			rocketaudio.stop()
+			mainbooster.emitting = false
 		if Input.is_action_pressed("rotateleft"):
 			apply_torque(Vector3(0.0,0.0,delta*torque_thrust))
+			rightboost.emitting = true
+		else:
+			rightboost.emitting = false
 		if Input.is_action_pressed("rotateright"):
 			apply_torque(Vector3(0.0,0.0,-delta*torque_thrust))
+			leftboost.emitting = true
+		else:
+			leftboost.emitting = false
 
 
 
@@ -48,6 +60,9 @@ func crash_sequence():
 	
 	transitioning = true
 	freeze = true
+	leftboost.emitting = false
+	mainbooster.emitting = false
+	rightboost.emitting = false
 	explosionaudio.play()
 	await get_tree().create_timer(2.45).timeout
 	
